60ada3d03ef5c379b54f996ee105aeb601d8251c
Daniel Liu <daniel17liu@gmail.com>
2022-12-01 09:15:58 -0800

    Formatting in backend

--- app-server/model/Gsi.js
+++ app-server/model/Gsi.js
@@ -3,15 +3,15 @@ const mongoose = require("mongoose");
 const GsiSchema = mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
-    required: false
+    required: false,
   },
   linkedin: {
     type: String,
-    required: false 
+    required: false,
   },
   createdAt: {
     type: Date,
@@ -19,38 +19,59 @@ const GsiSchema = mongoose.Schema({
   },
   rating: {
     type: Number,
-    required: false
+    required: false,
   },
   ratingCount: {
     type: Number,
-    required: false
+    required: false,
   },
   classesTaught: {
-    type: String,
-    required: false
+    type: Array,
+    required: false,
   },
   pronouns: {
     type: String,
-    required: false
+    required: false,
   },
   major: {
     type: String,
-    required: false
+    required: false,
   },
   semesters: {
     type: Array,
-    required: true
+    required: true,
   },
   comments: {
     type: Array,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 
 // export model gsi with GsiSchema
-module.exports = mongoose.model('GSI', GsiSchema)
+module.exports = mongoose.model("GSI", GsiSchema);
 
 new module.exports({
   name: "Mackenzie Moffit",
-  email: "hello@ca.edu"
-}).save()
+  email: "hello@ca.edu",
+  linkedin: "linkedin.com",
+  rating: 4,
+  ratingCount: 20,
+  classesTaught: ["math"],
+  pronouns: "aksdjf",
+  major: "asdf",
+  semesters: ["1", "2"],
+  comments: [
+    {
+      id: 1,
+      name: "joe",
+      rating: 5,
+      review: "good teacher",
+    },
+    {
+      id: 2,
+      name: "jo",
+      rating: 1,
+      review: "bad teacher",
+    },
+  ],
+}).save();
--- app-server/server.js
+++ app-server/server.js
@@ -1,108 +1,79 @@
 // To start the server:
 // cd app-server
-// node server.js  
+// node server.js
 
-const express = require('express')
-const bodyParser = require("body-parser")
-const mongoose = require('mongoose')
-const app = express()
-const port = 4000
-const user = require("./routes/user")
-const gsi = require("./model/Gsi")
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+const port = 4000;
+const user = require("./routes/user");
+const gsi = require("./model/Gsi");
 
-mongoose.connect('mongodb://localhost:27017/RateMyGSI-Database')
-const database = mongoose.connection
+mongoose.connect("mongodb://127.0.0.1:27017/RateMyGSI-Database");
+const database = mongoose.connection;
 
-// const gsiSchema = mongoose.Schema({
-//   name: {
-//     required: true,
-//     type: String
-//   },
-//   email: {
-//     required: false,
-//     type: String
-//   },
-//   linkedin: {
-//     required: false,
-//     type: String
-//   },
-//   createdAt: {
-//     type: Date,
-//     default: Date.now(),
-//   }
-// })
+database.on("connected", () => {
+  console.log("Connected to db successfully");
+});
 
-// const GSIModel = mongoose.model('GSI', gsiSchema)
+database.on("error", (err) => {
+  console.error("db error", err);
+});
 
-
-// new GSIModel({
-//   name: "Mackenzie Moffit",
-//   email: "hello@cal.edu"
-// }).save()
-
-
-database.on('connected', () => {
-  console.log('Connected to db successfully')
-})
-
-database.on('error', (err) => {
-  console.error("db error", err)
-})
-
-app.get('/gsis', async (req, res) => {
-  const data = await gsi.find()
-  res.json(data)
-})
+app.get("/gsis", async (req, res) => {
+  const data = await gsi.find();
+  res.json(data);
+});
 
 app.use(bodyParser.json());
 
 //Add new GSI to database
 app.post("/gsi/post", async (req, res) => {
-        console.log(req.body);
-        var newGsi = new gsi({name: req.body.name, email: req.body.email, linkedin: req.body.linkedin, rating: req.body.rating, ratingCount: req.body.ratingCount, classesTaught: req.body.classesTaught, pronouns: req.body.pronouns, major: req.body.major, semesters: req.body.semesters, ratings: []});
-        newGsi.save((err, res) => {
-            if (err) res.send(console.error(err));
-            res.send(res.name + " saved to database.");
-        });
-    }
-)
+  // console.log(req.body);
+  var newGsi = new gsi({
+    name: req.body.name,
+    email: req.body.email,
+    linkedin: req.body.linkedin,
+    rating: req.body.rating,
+    ratingCount: req.body.ratingCount,
+    classesTaught: req.body.classesTaught,
+    pronouns: req.body.pronouns,
+    major: req.body.major,
+    semesters: req.body.semesters,
+    ratings: [],
+  });
+  newGsi.save((err, res) => {
+    if (err) res.send(console.error(err));
+    res.send(res.name + " saved to database.");
+  });
+});
+
+//Get gsis by gsiid
+app.get("/gsi/:gsiid", async (req, res) => {
+  var curGsi = await gsi.findById(req.params.gsiid);
+  res.json(curGsi);
+});
 
 //Add new comment to the GSI with gsiid, requires request with a comment
-app.post("/comment/:gsiid/post", async(req, res) =>{
+app.post("/comment/:gsiid/post", async (req, res) => {
   var curGsi = await gsi.findById(req.params.gsiid);
   // console.log(curGsi.comments);
   if (!curGsi.comments) {
     curGsi.comments = [req.body.comment];
-  }
-  else {
+  } else {
     curGsi.comments = [...curGsi.comments, req.body.comment];
   }
   await curGsi.save();
   res.send("Comment added successfully");
-})
+});
 
 // Get all comment of a GSI with gsiid
-app.get("/comment/:gsiid", async(req, res) =>{
+app.get("/comment/:gsiid", async (req, res) => {
   var curGsi = await gsi.findById(req.params.gsiid);
-  res.json(curGsi.comments)
-})
-
+  res.json(curGsi.comments);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-app.post('/post', (req, res) => {
-  if (data[req.body.id]) {
-    throw new Error("Comment exists!")
-  }
-  const newGsi = {id:parseInt(req.body.id, 10), title: req.body.title, body: req.body.body, comments: []}
-  data[req.body.id] = newPost
-  res.send(newPost)
-})
-
-app.post('/post/:postId/comment', (req, res) => {
-  const post = data[req.params.postId]
-  post.comments = [...post.comments, req.body.newComment]
-  res.send(post)
-})
+  console.log(`Example app listening on port ${port}`);
+});
