dfd693f80491b92c80dbb26b8e328edc2cefea85
Daniel Liu <daniel17liu@gmail.com>
2022-12-01 17:49:40 -0800

    Added gsi submission functionality

--- app/src/components/submission/Submission.js
+++ app/src/components/submission/Submission.js
@@ -4,14 +4,60 @@ import {
   Checkbox,
   FormControl,
   FormControlLabel,
-  InputLabel,
-  Select,
   TextField,
 } from "@mui/material";
 import "../../css/Submission.css";
 import Navbar from "../shared/Navbar";
+import { useState } from "react";
+import axios from "axios";
 
 function Submission() {
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [classes, setClasses] = useState();
+  const [linkedin, setLinkedin] = useState();
+  const [email, setEmail] = useState();
+  const [pronouns, setPronouns] = useState();
+  const [semester, setSemester] = useState();
+  const [major, setMajor] = useState();
+  const [check, setCheck] = useState(false);
+  const [error, setError] = useState();
+
+  const submit = () => {
+    if (
+      firstName &&
+      lastName &&
+      classes &&
+      linkedin &&
+      email &&
+      pronouns &&
+      check
+    ) {
+      let classesTaught = classes.split(", ");
+      let semesters = semester.split(", ");
+      axios
+        .post("http://localhost:4000/gsi/post", {
+          name: `${firstName} ${lastName}`,
+          email: email,
+          linkedin: linkedin,
+          rating: 0,
+          ratingCount: 0,
+          classesTaught: classesTaught,
+          pronouns: pronouns,
+          major: major,
+          semesters: semesters,
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      setError("Please fill out all of the required areas");
+    }
+  };
+
   return (
     <div>
       <Navbar></Navbar>
@@ -26,23 +72,78 @@ function Submission() {
           listed on this website.
         </p>
         <FormControl className="form-container">
-          <TextField required label="GSI First Name" margin="normal" />
-          <TextField required label="GSI Last Name" margin="normal" />
-          <FormControl required margin="normal">
-            <InputLabel>Class</InputLabel>
-            <Select autoWidth label="Class *"></Select>
-          </FormControl>
-          <TextField label="LinkedIn" margin="normal" />
-          <TextField label="Email" margin="normal" />
+          <TextField
+            required
+            label="GSI First Name"
+            margin="normal"
+            onChange={(event) => {
+              setFirstName(event.target.value);
+            }}
+          />
+          <TextField
+            required
+            label="GSI Last Name"
+            margin="normal"
+            onChange={(event) => {
+              setLastName(event.target.value);
+            }}
+          />
+          <TextField
+            required
+            label="Class (comma separated)"
+            margin="normal"
+            onChange={(event) => {
+              setClasses(event.target.value);
+            }}
+          />
+          <TextField
+            label="LinkedIn"
+            margin="normal"
+            onChange={(event) => {
+              setLinkedin(event.target.value);
+            }}
+          />
+          <TextField
+            label="Email"
+            margin="normal"
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+          <TextField
+            label="Pronouns"
+            margin="normal"
+            onChange={(event) => {
+              setPronouns(event.target.value);
+            }}
+          />
+          <TextField
+            label="Semesters Taught (comma separated)"
+            margin="normal"
+            onChange={(event) => {
+              setSemester(event.target.value);
+            }}
+          />
+          <TextField
+            label="Major"
+            margin="normal"
+            onChange={(event) => {
+              setMajor(event.target.value);
+            }}
+          />
           <FormControlLabel
             control={<Checkbox size="small" />}
             label="I have checked that the data is accurate."
             margin="normal"
+            onChange={(event) => {
+              setCheck(event.target.checked);
+            }}
           ></FormControlLabel>
-          <Button variant="contained" margin="normal">
+          <Button variant="contained" margin="normal" onClick={submit}>
             Submit
           </Button>
         </FormControl>
+        <div>{error}</div>
       </div>
     </div>
   );
